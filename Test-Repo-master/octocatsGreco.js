27958539a1e87a43b3c0a5dc52047294e21814b9
AnthonyGreco20 <9agreco@stpatrick.org>
2016-10-07 12:54:49 -0500

    Worked cleaning up code and figuring out mistakes.

---
+++ Test-Repo-master/octocatsGreco.js
@@ -0,0 +1,21 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n, a){
+  this.name = n
+  this.arm = a
+  this.slap=function(){
+      for(a=0;a<9;a++){
+      console.log("SLAP");
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+  //Write your 3 new octocat objects below here.
